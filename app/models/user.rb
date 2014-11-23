26cd5b8f86459f546388337df4e954c7da2c5ba1
Rafael Barboza <barboza.cardoso@gmail.com>
2014-11-23 14:26:01 -0200

    Fix the access type on the registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
 
   validates_length_of :bio, maximum: 140
 
-  validates_presence_of :email
+  validates_presence_of :email, :access_type
   validates_uniqueness_of :email, allow_blank: true, if: :email_changed?, message: I18n.t('activerecord.errors.models.user.attributes.email.taken')
   validates_format_of :email, with: Devise.email_regexp, allow_blank: true, if: :email_changed?
 
