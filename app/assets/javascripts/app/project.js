151bee0881e8e94a70d00c7be5f10b564a08135a
Rafael Barboza <barboza.cardoso@gmail.com>
2014-12-15 12:09:44 -0200

    Add the metrics page

--- app/assets/javascripts/app/project.js
+++ app/assets/javascripts/app/project.js
@@ -18,7 +18,7 @@ App.addChild('Project', _.extend({
     this.route('comments');
     this.route('edit');
     this.route('reports');
-    this.route('metrics');
+    this.route('project_metrics');
   },
 
   toggleWarning: function(){
@@ -37,7 +37,7 @@ App.addChild('Project', _.extend({
     if($tab.length > 0){
       this.onTabClick({ currentTarget: $tab });
 
-      if(($tab.prop('id') == 'metrics_link') || ($tab.prop('id') == 'basics_link') || ($tab.prop('id') == 'dashboard_project_link') || ($tab.prop('id') == 'dashboard_reward_link')) {
+      if(($tab.prop('id') == 'project_metrics_link') || ($tab.prop('id') == 'basics_link') || ($tab.prop('id') == 'dashboard_project_link') || ($tab.prop('id') == 'dashboard_reward_link')) {
         $('#project-sidebar').hide();
       } else {
         $('#project-sidebar').show();
