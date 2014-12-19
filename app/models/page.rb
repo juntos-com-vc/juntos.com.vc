06c81174273e120bd2ad2c1a89d7dfa613767cee
Enzo Zuccolotto <enzozuccolotto@gmail.com>
2014-12-19 17:02:42 -0200

    Add pages crud localizations.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,9 @@
 class Page < ActiveRecord::Base
+  include Shared::CatarseAutoHtml
 
   validates_presence_of :content
   enum name: [:who_we_are]
+
+  catarse_auto_html_for field: :content, video_width: 600, video_height: 403
 
 end
