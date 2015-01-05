8cf5ea2bf1eb9c3d96b5723fcd70d48ac735d9ed
Enzo Zuccolotto <enzozuccolotto@gmail.com>
2015-01-05 14:48:18 -0200

    [WIP] Transparency tab on who we are page.

--- app/assets/javascripts/app/who_we_are.js
+++ app/assets/javascripts/app/who_we_are.js
@@ -3,7 +3,8 @@ App.addChild('WhoWeAre', {
 
   events: {
     'click a#history_link' : 'toggleHistoryContent',
-    'click a#people_link' : 'togglePeopleContent'
+    'click a#people_link' : 'togglePeopleContent',
+    'click a#transparency_link' : 'toggleTransparencyContent'
   },
 
   activate: function() {
@@ -11,20 +12,38 @@ App.addChild('WhoWeAre', {
     this.$historyLink = $('#history_link');
     this.$people = $('#who_we_are_people');
     this.$peopleLink = $('#people_link');
+    this.$transparency = $('#who_we_are_transparency');
+    this.$transparencyLink = $('#transparency_link');
   },
 
   toggleHistoryContent: function(){
     this.$history.removeClass('w-lightbox-hide');
-    this.$people.addClass('w-lightbox-hide');
     this.$historyLink.addClass('selected');
+
+    this.$people.addClass('w-lightbox-hide');
     this.$peopleLink.removeClass('selected');
+    this.$transparency.addClass('w-lightbox-hide');
+    this.$transparencyLink.removeClass('selected');
   },
 
   togglePeopleContent: function(){
     this.$people.removeClass('w-lightbox-hide');
-    this.$history.addClass('w-lightbox-hide');
     this.$peopleLink.addClass('selected');
+
+    this.$history.addClass('w-lightbox-hide');
     this.$historyLink.removeClass('selected');
+    this.$transparency.addClass('w-lightbox-hide');
+    this.$transparencyLink.removeClass('selected');
+  },
+
+  toggleTransparencyContent: function(){
+    this.$transparency.removeClass('w-lightbox-hide');
+    this.$transparencyLink.addClass('selected');
+
+    this.$history.addClass('w-lightbox-hide');
+    this.$historyLink.removeClass('selected');
+    this.$people.addClass('w-lightbox-hide');
+    this.$peopleLink.removeClass('selected');
   }
 
 });
