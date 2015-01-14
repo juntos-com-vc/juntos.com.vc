eb16ded2766e8416fca18a4afe67fe2b9bfe0410
Daniel Weinmann <danielweinmann@gmail.com>
2015-01-14 15:35:36 -0200

    Changes blog feed url

--- lib/blog.rb
+++ lib/blog.rb
@@ -3,7 +3,7 @@ class Blog
     def fetch_last_posts
       Rails.cache.fetch('blog_posts', expires_in: 10.minutes) do
         begin
-          feed = Feedjira::Feed.fetch_and_parse("#{CatarseSettings[:blog_url]}?feed=rss2")
+          feed = Feedjira::Feed.fetch_and_parse(CatarseSettings[:blog_feed_url])
           feed.entries
         rescue
           []
