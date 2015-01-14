ee6a4c52251b3240fa1e73a19a052b690d7722b4
Daniel Weinmann <danielweinmann@gmail.com>
2015-01-14 17:43:57 -0200

    Makes projects admin visible to channel trustees

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,11 +1,11 @@
-class Admin::ProjectsController < Admin::BaseController
+class Admin::ProjectsController < Channels::Admin::BaseController
   layout 'juntos_bootstrap'
 
   has_scope :by_user_email, :by_id, :pg_search, :user_name_contains, :with_state, :by_category_id, :order_by, :by_channel
   has_scope :between_created_at, :between_expires_at, :between_online_date, :between_updated_at, :goal_between, using: [ :start_at, :ends_at ]
 
   before_filter do
-    @total_projects = Project.count(:all)
+    @total_projects = collection.count(:all)
   end
 
   [:approve, :reject, :push_to_draft, :push_to_trash].each do |name|
@@ -28,6 +28,9 @@ class Admin::ProjectsController < Admin::BaseController
   protected
   def collection
     @scoped_projects = apply_scopes(end_of_association_chain).with_project_totals.without_state('deleted')
+    if channel
+      @scoped_projects = @scoped_projects.where("id IN (SELECT project_id FROM channels_projects WHERE channel_id = #{channel.id})")
+    end
     @projects = @scoped_projects.page(params[:page])
   end
 end
