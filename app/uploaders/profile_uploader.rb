33d6aa6f285321e99359622da44aca1dd679ddf2
Rafael Barboza <barboza.cardoso@gmail.com>
2015-01-21 14:21:47 -0200

    Make the channel logo png

--- app/uploaders/profile_uploader.rb
+++ app/uploaders/profile_uploader.rb
@@ -4,17 +4,14 @@ class ProfileUploader < ImageUploader
 
   version :curator_thumb do
     process resize_to_limit: [1200,630]
-    process convert: :jpg
   end
 
   version :slick do
     process resize_to_fill: [1900,360]
-    process convert: :jpg
   end
 
   version :header do
     process resize_to_fill: [176, 38]
-    process convert: :jpg
   end
 
 end
