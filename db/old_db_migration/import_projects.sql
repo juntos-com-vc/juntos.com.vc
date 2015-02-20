215d140ff8c7c11ecedd078dd71ee057c684d35d
Rafael Barboza <barboza.cardoso@gmail.com>
2015-02-19 22:58:57 -0200

    Import the Garupa projects

--- db/old_db_migration/import_projects.sql
+++ db/old_db_migration/import_projects.sql
@@ -65,3 +65,77 @@ JOIN categories
   ON categories.uuid = old_projects."categoriaprojetoId"
 WHERE ativo = 'true'
   AND "dataAtivacao" is not null;
+
+
+/* Add the Garupa category */
+INSERT INTO categories (
+  name_pt,
+  created_at,
+  updated_at,
+  color
+) VALUES (
+  'Garupa',
+  '2013-01-01 00:00:00',
+  '2013-01-01 00:00:00',
+  '#b8292a'
+);
+
+/* Add the garupa channel */
+INSERT INTO channels (
+  name,
+  permalink,
+  description,
+  created_at,
+  updated_at,
+  category_id
+) VALUES (
+  'Garupa',
+  'garupa',
+  'Canal garupa',
+  '2013-01-01 00:00:00',
+  '2013-01-01 00:00:00',
+  (select id from categories where name_pt = 'Garupa')
+);
+
+/* Import garupa projects */
+INSERT INTO projects (
+  uuid,
+  category_id,
+  user_id,
+  name,
+  headline,
+  goal,
+  permalink,
+  online_days,
+  about,
+  about_html,
+  video_url,
+  thank_you,
+  created_at,
+  updated_at,
+  online_date,
+  state
+) SELECT
+  "projetoId",
+  categories.id,
+  users.id,
+  nome,
+  substr(descricao, 0, 140),
+  ("metaValor"::float::integer),
+  url,
+  tempo::integer,
+  "descricao",
+  "descricao",
+  video,
+  agradecimento,
+  "dataCadastro",
+  "dataAlteracao",
+  "dataAtivacao",
+  'online'
+FROM "old_db"."garupa_projeto" as old_projects
+JOIN users
+  ON users.email = (SELECT email FROM old_db.garupa_cliente WHERE old_db.garupa_cliente."clienteId" = old_projects."clienteId")
+JOIN categories
+  ON categories.name_pt = 'Garupa'
+WHERE ativo = 'true'
+  AND "dataAtivacao" is not null;
