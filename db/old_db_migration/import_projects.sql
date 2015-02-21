9146372f996b510d36f4bcca7b0b606d63b7637c
Rafael Barboza <barboza.cardoso@gmail.com>
2015-02-21 19:26:49 -0200

    Import the drafts

--- db/old_db_migration/import_projects.sql
+++ db/old_db_migration/import_projects.sql
@@ -24,7 +24,7 @@ UPDATE old_db.juntoscomvc_projeto SET url = 'brincarumdireitodacrianca-2' WHERE 
 UPDATE old_db.juntoscomvc_projeto SET url = 'cartaelivro-2' WHERE "projetoId" = '58E96A45-B600-4D1C-A0A1-2C274000CE3B';
 UPDATE old_db.juntoscomvc_projeto SET url = 'ios-1' WHERE "projetoId" = 'DE8404A9-B92E-4112-AD59-ACA0BE9F47C5';
 
-/* Import projects */
+/* Import online projects */
 INSERT INTO projects (
   uuid,
   category_id,
@@ -66,6 +66,52 @@ JOIN categories
   ON categories.uuid = old_projects."categoriaprojetoId"
 WHERE ativo = 'true'
   AND "dataAtivacao" is not null;
+
+/* Import drafts */
+INSERT INTO projects (
+  uuid,
+  category_id,
+  user_id,
+  name,
+  headline,
+  goal,
+  permalink,
+  online_days,
+  about,
+  about_html,
+  video_url,
+  thank_you,
+  created_at,
+  updated_at,
+  online_date,
+  state
+) SELECT
+  "projetoId",
+  categories.id,
+  users.id,
+  nome,
+  CASE WHEN descricao != '' then substr(descricao, 0, 140) else 'placeholder headline' end,
+  ("metaValor"::float::integer),
+  CASE WHEN url != '' THEN url ELSE "projetoId" END,
+  tempo::integer,
+  CASE WHEN "descricao" != '' THEN descricao ELSE 'placeholder about' END,
+  CASE WHEN "descricao" != '' THEN descricao ELSE 'placeholder agout' END,
+  video,
+  agradecimento,
+  "dataCadastro",
+  "dataAlteracao",
+  "dataAtivacao",
+  'draft'
+FROM "old_db"."juntoscomvc_projeto" as old_projects
+JOIN users
+  ON users.uuid = old_projects."clienteId"
+JOIN categories
+  ON categories.uuid = old_projects."categoriaprojetoId"
+WHERE ativo = 'true'
+  AND "dataAtivacao" is null
+  AND "dataAvaliacaoPasso2" > '2013-01-01';
+
+
 
 
 /* Add the Garupa category */
@@ -140,3 +186,47 @@ JOIN categories
   ON categories.name_pt = 'Garupa'
 WHERE ativo = 'true'
   AND "dataAtivacao" is not null;
+
+/* Import garupa drafts */
+INSERT INTO projects (
+  uuid,
+  category_id,
+  user_id,
+  name,
+  headline,
+  goal,
+  permalink,
+  online_days,
+  about,
+  about_html,
+  video_url,
+  thank_you,
+  created_at,
+  updated_at,
+  online_date,
+  state
+) SELECT
+  "projetoId",
+  categories.id,
+  users.id,
+  nome,
+  substr(descricao, 0, 140),
+  ("metaValor"::float::integer),
+  url,
+  tempo::integer,
+  "descricao",
+  "descricao",
+  video,
+  agradecimento,
+  "dataCadastro",
+  "dataAlteracao",
+  "dataAtivacao",
+  'draft'
+FROM "old_db"."garupa_projeto" as old_projects
+JOIN users
+  ON users.email = (SELECT email FROM old_db.garupa_cliente WHERE old_db.garupa_cliente."clienteId" = old_projects."clienteId")
+JOIN categories
+  ON categories.name_pt = 'Garupa'
+WHERE ativo = 'true'
+  AND "dataAtivacao" is null
+  AND "dataAvaliacaoPasso2" > '2013-01-01';
