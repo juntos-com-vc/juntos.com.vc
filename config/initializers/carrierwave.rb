3fd1ae4a6056b0cc3ef524bde679eadf6ed9c4f6
Rafael Barboza <barboza.cardoso@gmail.com>
2015-03-06 09:58:46 -0300

    Fix the aws bucket region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       host: 's3.amazonaws.com',
-      endpoint: 'http://juntoscomvc.s3.amazonaws.com',
       aws_access_key_id: CatarseSettings.get_without_cache(:aws_access_key),
-      aws_secret_access_key: CatarseSettings.get_without_cache(:aws_secret_key)
+      aws_secret_access_key: CatarseSettings.get_without_cache(:aws_secret_key),
+      region: 'sa-east-1'
     }
     config.fog_directory  = CatarseSettings.get_without_cache(:aws_bucket)
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
