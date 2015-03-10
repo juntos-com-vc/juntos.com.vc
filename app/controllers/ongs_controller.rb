7dd54ed0c3124814cc8bd573424347f94bae9568
Rafael Barboza <barboza.cardoso@gmail.com>
2015-03-09 23:07:26 -0300

    Bugs

--- app/controllers/ongs_controller.rb
+++ app/controllers/ongs_controller.rb
@@ -3,7 +3,7 @@ class OngsController < ApplicationController
 
   def index
     ongs = User.where(access_type: User.access_types[:legal_entity]).order(name: :asc)
-    @ongs = ongs.group_by { |ong| ong.name[0].upcase }
+    @ongs = ongs.group_by { |ong| ong.name[0].try(:upcase) }
   end
 
 end
