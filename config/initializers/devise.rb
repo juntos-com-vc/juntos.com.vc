8366c6db5f3a298c0ee602e047b91352b3b66be7
Rafael Barboza <barboza.cardoso@gmail.com>
2015-03-12 23:04:46 -0300

    Fix the devise sender email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "Catarse <system@catarse.me>"
+  config.mailer_sender = "Juntos com vc <contato@juntos.com.vc>"
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
