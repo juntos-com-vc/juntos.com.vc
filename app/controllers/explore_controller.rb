fe6ca60c6074b40724797ebdeb094d7efcd549b8
Rafael Barboza <barboza.cardoso@gmail.com>
2015-03-15 00:16:17 -0300

    Makes the explore inacessible from channels

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,5 +1,6 @@
 class ExploreController < ApplicationController
   def index
+    redirect_to root_url if channel.present?
     @title = t('explore.title')
 
     @categories = Category.with_projects.order(:name_pt).all
