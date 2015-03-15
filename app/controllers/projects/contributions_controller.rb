f3645d282f06853e727eb6b4e1fe5a7374eea285
Rafael Barboza <barboza.cardoso@gmail.com>
2015-03-14 23:52:39 -0300

    Remove the minimum value from contributions

--- app/controllers/projects/contributions_controller.rb
+++ app/controllers/projects/contributions_controller.rb
@@ -41,7 +41,6 @@ class Projects::ContributionsController < ApplicationController
 
   def new
     @contribution = Contribution.new(project: parent, user: current_user)
-    @contribution.project_value = 10
     authorize @contribution
 
     @title = t('projects.contributions.new.title', name: @project.name)
