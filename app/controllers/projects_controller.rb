cbaee2a1b4edd1b876613b4df3cb9ec2e9ef2133
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2015-04-17 11:54:25 -0300

    Ignore HomeBanners without image in layout

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -28,7 +28,7 @@ class ProjectsController < ApplicationController
           @regular_partners = SitePartner.regular
           @site_partners = @featured_partners + @regular_partners
           @channels = Channel.order("random()").limit(5)
-          @banners = HomeBanner.all
+          @banners = HomeBanner.where.not(image: [nil, ''])
         end
       end
     end
