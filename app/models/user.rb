7968b9aef87aa81d9b08b682034caba4578ca928
Rafael Barboza <barboza.cardoso@gmail.com>
2015-05-09 18:03:15 -0300

    Fix the users with credits report

--- app/models/user.rb
+++ app/models/user.rb
@@ -104,7 +104,7 @@ class User < ActiveRecord::Base
   scope :by_name, ->(name){ where('users.name ~* ?', name) }
   scope :by_id, ->(id){ where(id: id) }
   scope :by_key, ->(key){ where('EXISTS(SELECT true FROM contributions WHERE contributions.user_id = users.id AND contributions.key ~* ?)', key) }
-  scope :has_credits, -> { joins(:user_total).where('user_totals.credits > 0') }
+  scope :has_credits, -> { where('user_totals.credits > 0') }
   scope :only_organizations, -> { where(access_type: 1) }
   scope :already_used_credits, -> {
     has_credits.
