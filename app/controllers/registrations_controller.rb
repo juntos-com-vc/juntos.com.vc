ddd7b6a71b52b2a9e718329d9ea574ed651e8ddc
Bruno Pagno <brunopagno@gmail.com>
2015-09-30 18:54:40 -0300

    Changed registration redirect if user is backing a campaign

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,13 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    "/#{I18n.locale}/sign_up_success"
+    return_to = session[:return_to]
+    if return_to and return_to.include?('contributions')
+      return_to = session[:return_to]
+      session[:return_to] = nil
+      return return_to
+    else
+      return "/#{I18n.locale}/sign_up_success"
+    end
   end
 end
