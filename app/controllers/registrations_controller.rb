89bda5772f4d508d43773e8bdf72f48731bf8ee4
Bruno Pagno <brunopagno@gmail.com>
2015-10-07 16:19:23 -0300

    Fixed missing tags

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
+    # add session information for tags
+    session[:new_user_registration] = true;
+
+    # normal after_sign_up_path_for
     return_to = session[:return_to]
     if return_to and return_to.include?('contributions')
       return_to = session[:return_to]
