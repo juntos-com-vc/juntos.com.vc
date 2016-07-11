8ea6c7599409743f89584b7ec19250824fe557b3
Andreza Medeiros <andreza.cmedeiros@gmail.com>
2016-07-11 17:53:20 -0300

    Add endpoint in rollbar initializer

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -4,6 +4,8 @@ Rollbar.configure do |config|
 
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
+  config.endpoint =  ENV['ROLLBAR_ENDPOINT']
+
   # Here we'll disable in 'test':
   if Rails.env.test?
     config.enabled = false
