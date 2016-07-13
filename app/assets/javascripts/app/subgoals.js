2777003312d9f07751dccdf1fbb0896a16809fc1
matheus <matheuscla@yahoo.com.br>
2016-07-13 12:49:53 -0300

    Add Progress Bar

--- app/assets/javascripts/app/subgoals.js
+++ app/assets/javascripts/app/subgoals.js
@@ -8,3 +8,14 @@ $(document).ready(function() {
   });
   $(".project-progress-stats").css("margin-bottom", (absolute_height - 20) + "px");
 });
+
+$(document).ready(function() {
+  var absolute_height = 0;
+  $(".progress-bar").each(function(i, e) {
+    var eheight = $(e).innerHeight();
+    if (eheight > absolute_height) {
+      absolute_height = eheight;
+    }
+  });
+  $(".project-progress-stats").css("margin-bottom", (absolute_height - 20) + "px");
+});
