58aa95362deedaeee61ac2b6845ee753c6957a5b
matheus <matheuscla@yahoo.com.br>
2016-08-08 14:02:10 -0300

    Add donate button, mobile header link

--- app/assets/javascripts/app/subgoals.js
+++ app/assets/javascripts/app/subgoals.js
@@ -9,6 +9,6 @@ $(document).ready(function() {
   $(".project-progress-stats").css("margin-bottom", (absolute_height - 20) + "px");
 
   if ($(window).width() < 991){
-    $(".project-progress-stats").css("margin-bottom", (absolute_height - 130) + "px");
+    $(".project-progress-stats").css("margin-bottom", (absolute_height - 230) + "px");
   }
 });
