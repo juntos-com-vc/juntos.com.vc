5397a2d3bbaead6290f36ca182d39c6087d643c9
Igor Fernandes <igorrfc10@gmail.com>
2016-11-07 19:02:56 -0300

    Create RecurringContribution's update plans service
    This service will synchronize all PagarMe's API Plans made for Junto's recurring contributions in the application's database.

---
+++ app/services/recurring_contribution/update_plans.rb
@@ -0,0 +1,34 @@
+class RecurringContribution::UpdatePlans
+  def self.call
+    new.call
+  end
+
+  def call
+    Plan.transaction do
+      pagarme_plans.map do |plan|
+        build_plan(plan) unless plan_exist?(plan)
+      end.compact
+    end
+  end
+
+  private
+
+  def build_plan(plan)
+    normalize_payment_methods(plan)
+    Plan.create(plan_code: plan.id, name: plan.name, amount: plan.amount, payment_methods: plan.payment_methods)
+  end
+
+  def normalize_payment_methods(plan)
+    if plan.payment_methods.include? 'boleto'
+      plan.payment_methods.each { |payment_method| payment_method.gsub!('boleto', 'bank_billet') }
+    end
+  end
+
+  def pagarme_plans
+    PagarMe::Plan.all
+  end
+
+  def plan_exist?(plan)
+    Plan.exists?(plan_code: plan.id)
+  end
+end
