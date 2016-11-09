9fd99bd9fdcbf33834e5867d55f855515ebb430f
Igor Fernandes <igorrfc10@gmail.com>
2016-11-09 15:45:00 -0300

    Add plan_code attr to Plan model

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,5 @@
 class Plan < ActiveRecord::Base
-  attr_accessible :name, :amount, :payment_methods
+  attr_accessible :plan_code, :name, :amount, :payment_methods
 
   extend Enumerize
   extend ActiveModel::Naming
@@ -8,5 +8,5 @@ class Plan < ActiveRecord::Base
             in: { credit_card: 0, bank_billet: 1 },
             multiple: true, i18n_scope: "enumerize.plan.payment_methods"
 
-  validates_presence_of :name, :amount, :payment_methods
+  validates_presence_of :plan_code, :name, :amount, :payment_methods
 end
