a15776b4c2565fc3042606ea1505b1e4dc38c3db
Maychell <maychellfernandes@hotmail.com>
2016-11-23 10:40:41 -0300

    fixed projects query to get only successful projects for statistics

--- app/controllers/channels/profiles_controller.rb
+++ app/controllers/channels/profiles_controller.rb
@@ -22,8 +22,10 @@ class Channels::ProfilesController < Channels::BaseController
 
   private
   def show_statistics
-    @total_pledged = resource.projects.map(&:project_total).compact.sum(&:pledged).to_f
-    @total_projects = resource.projects.count
-    @total_contributions = resource.projects.joins(:contributions).count
+    successful_projects = resource.projects.with_state('successful')
+
+    @total_pledged = successful_projects.map(&:project_total).compact.sum(&:pledged).to_f
+    @total_projects = successful_projects.count
+    @total_contributions = successful_projects.joins(:contributions).count
   end
 end
