c7075636c1c87890078a0a25f05c9955eee08c5b
Igor Fernandes <igorrfc10@gmail.com>
2016-12-05 17:10:24 -0300

    Update Channels:ProfilesController specs
    removing all statistics calculations.

--- spec/controllers/channels/profiles_controller_spec.rb
+++ spec/controllers/channels/profiles_controller_spec.rb
@@ -1,37 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Channels::ProfilesController, type: :controller do
-  let (:channel) { create(:channel) }
-  let (:project) { create(:project) }
-  let! (:contribution) { create(:contribution, value: 10, project: project) }
-  let! (:contribution2) { create(:contribution, value: 10, project: project) }
-
   describe "GET show" do
-    before do
-      project.update_attribute(:state, 'successful')
-      channel.projects << project
-      allow(request).to receive(:subdomain).and_return(channel.permalink)
-      get :show, id: 'sample', locale: 'pt'
-    end
+    let (:channel) { create(:channel) }
 
     it 'should return status 200' do
+      allow(request).to receive(:subdomain).and_return(channel.permalink)
+      get :show, id: 'sample', locale: 'pt'
       expect(response).to be_success
     end
-
-    context 'Show statistics' do
-      it 'Counts how many projects has inside a channel' do
-        expect(assigns(:total_projects)).to eq(1)
-      end
-
-      it 'Count how many contributions a channel has' do
-        expect(assigns(:total_contributions)).to eq(2)
-      end
-
-      it 'Sum pledged from all projects inside a channel' do
-        expect(assigns(:total_pledged)).to eq(20)
-      end
-
-    end
   end
-
 end
