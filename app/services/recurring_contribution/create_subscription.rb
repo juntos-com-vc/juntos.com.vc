d1d8de8ff7a2694892e011f449b89cc114a8f356
Igor Fernandes <igorrfc10@gmail.com>
2016-12-14 15:49:55 -0300

    Add RecurringContribution::CreateSubscription service
    responsible for the creation of project subscriptions both on pagarme's API and juntos database.

---
+++ app/services/recurring_contribution/create_subscription.rb
@@ -0,0 +1,33 @@
+class RecurringContribution::CreateSubscription
+  def initialize(plan, project, user, payment_method, credit_card_hash)
+    @plan = plan
+    @project = project
+    @user = user
+    @payment_method = payment_method
+    @credit_card_hash = credit_card_hash
+  end
+
+  def self.process(plan:, project:, user:, payment_method:, credit_card_hash: nil)
+    new(plan, project, user, payment_method, credit_card_hash).process
+  end
+
+  def process
+    pagarme_response = create_subscription_on_pagarme
+    create_subscription_on_juntos(pagarme_response)
+  end
+
+  private
+  attr_reader :plan, :project, :user, :payment_method, :credit_card_hash
+
+  def create_subscription_on_pagarme
+    RecurringContribution::Subscriptions::Pagarme.new(plan.plan_code, user, payment_method, credit_card_hash).process
+  end
+
+  def create_subscription_on_juntos(pagarme_response)
+    juntos_subscription_service(project, pagarme_response).process
+  end
+
+  def juntos_subscription_service(project, pagarme_response)
+    RecurringContribution::Subscriptions::Juntos.new(project, pagarme_response)
+  end
+end
