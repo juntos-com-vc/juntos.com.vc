c903cad8c2cf662617558213d0e955da847c5caa
Igor Fernandes <igorrfc10@gmail.com>
2016-12-16 14:52:34 -0300

    Update Pagarme::API service
    creating the method 'valid_request_signature?' for verify if a request is made by PagarMe.

--- app/services/pagarme/api.rb
+++ app/services/pagarme/api.rb
@@ -7,5 +7,12 @@ class Pagarme::API
     def create_subscription(attributes)
       PagarMe::Subscription.create(attributes)
     end
+
+    def valid_request_signature?(request)
+      payload = request.raw_post
+      signature = request.headers['HTTP_X_HUB_SIGNATURE'] || ""
+
+      PagarMe::Postback.valid_request_signature?(payload, signature)
+    end
   end
 end
