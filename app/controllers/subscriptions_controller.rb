3f45b1fddf6fe2afc0d39c61a09a087f7c808cdd
Igor Fernandes <igorrfc10@gmail.com>
2016-12-19 17:43:59 -0300

    Update SubscriptionsController
    changing the used update status service for the RecurringContribution::UpdateStatus service.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,8 +1,9 @@
 class SubscriptionsController < ApplicationController
   def update_status
-    RecurringContribution::Subscriptions::UpdateStatus.process(request)
+    subscription = Subscription.find_by(subscription_code: params[:id])
+    RecurringContribution::UpdateStatus.process(request, subscription)
     render nothing: true, status: :ok
-  rescue RecurringContribution::Subscriptions::UpdateStatus::InvalidRequestError
+  rescue RecurringContribution::UpdateStatus::InvalidRequestError
     render json: { error: 'invalid postback' }, status: :bad_request
   end
 end
