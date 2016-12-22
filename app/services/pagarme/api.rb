85f3f7d906f5df0c262f6270191de99d001316fb
Igor Fernandes <igorrfc10@gmail.com>
2016-12-22 16:35:50 -0300

    Update Pagarme::API service
    adding the handling for the possible exceptions raised on the communication with the Pagarme's API.

--- app/services/pagarme/api.rb
+++ app/services/pagarme/api.rb
@@ -1,11 +1,23 @@
 class Pagarme::API
+  class ConnectionError       < StandardError; end
+  class ResourceNotFound      < StandardError; end
+  class InvalidAttributeError < StandardError; end
+
   class << self
     def find_plan(plan_id)
       PagarMe::Plan.find_by_id(plan_id)
+    rescue PagarMe::ConnectionError
+      raise_connection_error
+    rescue PagarMe::NotFound
+      raise ResourceNotFound
     end
 
     def create_subscription(attributes)
       PagarMe::Subscription.create(attributes)
+    rescue PagarMe::ConnectionError
+      raise_connection_error
+    rescue PagarMe::ValidationError => e
+      raise InvalidAttributeError, e.message
     end
 
     def valid_request_signature?(request)
@@ -13,6 +25,14 @@ class Pagarme::API
       signature = request.headers['HTTP_X_HUB_SIGNATURE'] || ""
 
       PagarMe::Postback.valid_request_signature?(payload, signature)
+    rescue PagarMe::ConnectionError
+      raise_connection_error
+    end
+
+    private
+
+    def raise_connection_error
+      raise ConnectionError, 'The connection with our payment server was lost'
     end
   end
 end
