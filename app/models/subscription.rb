26d02380d6a9f5e81baaf3b394b321e9bc9ce549
Igor Fernandes <igorrfc10@gmail.com>
2016-12-23 17:21:17 -0300

    Update Subscription model
    adding two new attributes: 'charging_day' and 'credit_card_key'. The 'credit_card_key' is a reference to the CreditCard created on Pagarme when a subscription paid with credit card is made. The charging day refer to the month day which the user will be charged by pagarme.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,15 +1,17 @@
 class Subscription < ActiveRecord::Base
   extend Enumerize
 
-  validates_presence_of :status, :payment_method,
+  validates_presence_of :status, :payment_method, :charging_day,
                         :plan_id, :user_id, :project_id
 
   validates_inclusion_of :payment_method, in: 'permitted_payment_methods',
                                           unless: 'plan.nil?'
 
+  validates_numericality_of :charging_day, less_than_or_equal_to: 28, greater_than: 0
+
   enumerize :payment_method, in: { credit_card: 0, bank_billet: 1 }
 
-  enumerize :status, in: { paid: 0, pending_payment: 1, unpaid: 2, canceled: 3 }
+  enumerize :status, in: { paid: 0, pending_payment: 1, unpaid: 2, canceled: 3, waiting_for_charging_day: 4 }
 
   has_many   :transactions
   belongs_to :user
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -2,34 +2,38 @@ require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
 
-  describe 'associations' do
+  describe "associations" do
     it{ is_expected.to belong_to :user }
     it{ is_expected.to belong_to :project }
     it{ is_expected.to belong_to :plan }
   end
 
-  describe 'validations' do
+  describe "validations" do
     let(:plan) { create(:plan, :with_credit_card) }
     let(:unpermitted_payment_subscription) { build(:subscription, :bank_billet_payment, plan: plan) }
     let(:permitted_payment_subscription) { build(:subscription, :credit_card_payment, plan: plan) }
 
     it { is_expected.to validate_presence_of(:status) }
+    it { is_expected.to validate_presence_of(:charging_day) }
     it { is_expected.to validate_presence_of(:payment_method) }
     it { is_expected.to validate_presence_of(:plan_id) }
     it { is_expected.to validate_presence_of(:user_id) }
     it { is_expected.to validate_presence_of(:project_id) }
 
+    it { is_expected.not_to allow_value(0).for(:charging_day) }
+    it { is_expected.not_to allow_value(29).for(:charging_day) }
+
     it { is_expected.to enumerize(:payment_method).in(:credit_card, :bank_billet) }
-    it { is_expected.to enumerize(:status).in(:paid, :pending_payment, :unpaid, :canceled) }
+    it { is_expected.to enumerize(:status).in(:paid, :pending_payment, :unpaid, :canceled, :waiting_for_charging_day) }
 
     context "when the subscription is made with a plan's unpermitted payment type" do
-      it 'should be invalid' do
+      it "should be invalid" do
         expect(unpermitted_payment_subscription).to be_invalid
       end
     end
 
-    context 'when the payment_method is permitted by the plan' do
-      it 'should be valid' do
+    context "when the payment_method is permitted by the plan" do
+      it "should be valid" do
         expect(permitted_payment_subscription).to be_valid
       end
     end
