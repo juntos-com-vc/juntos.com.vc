1d2eee665c5870e742d39ea6dcf17049420a16df
Igor Fernandes <igorrfc10@gmail.com>
2016-12-26 13:27:51 -0300

    Update RecurringContribution::Subscriptions::Create service
    changing the entry parameters to receive only the previously created juntos' subscription and pass it both for Subscriptions::CreatePagarme and Subscriptions::UpdateJuntos services. After this update, all the information needed for subscription creating will be centralized into the juntos' subscriptions gave to this service.

--- app/services/recurring_contribution/subscriptions/create_pagarme.rb
+++ app/services/recurring_contribution/subscriptions/create_pagarme.rb
@@ -1,6 +1,6 @@
 class RecurringContribution::Subscriptions::CreatePagarme
-  def initialize(juntos_subscription, credit_card = nil)
-    @credit_card = credit_card
+  def initialize(juntos_subscription)
+    @juntos_subscription = juntos_subscription
     @payment_method = normalize_payment_method(juntos_subscription.payment_method)
     @plan_id = juntos_subscription.plan.plan_code
     @user = juntos_subscription.user
@@ -11,23 +11,24 @@ class RecurringContribution::Subscriptions::CreatePagarme
   end
 
   private
+  attr_reader :juntos_subscription, :payment_method, :plan_id, :user
 
   def attributes
-    return default_attributes.merge(credit_card_attributes) if credit_card?
+    return default_attributes.merge(credit_card_id) if credit_card?
     default_attributes
   end
 
   def default_attributes
     {
-      plan: ::Pagarme::API.find_plan(@plan_id),
-      payment_method: @payment_method,
+      plan: ::Pagarme::API.find_plan(plan_id),
+      payment_method: payment_method,
       postback_url: postback_url,
-      customer: { email: @user.email }
+      customer: { email: user.email }
     }
   end
 
-  def credit_card_attributes
-    @credit_card.slice(:card_number, :card_holder_name, :card_expiration_month, :card_expiration_year, :card_cvv)
+  def credit_card_id
+    { card_id: juntos_subscription.credit_card_key }
   end
 
   def normalize_payment_method(payment_method)
@@ -35,11 +36,11 @@ class RecurringContribution::Subscriptions::CreatePagarme
   end
 
   def credit_card?
-    @payment_method == 'credit_card'
+    juntos_subscription.payment_method == 'credit_card'
   end
 
   def bank_billet?
-    @payment_method == 'bank_billet'
+    juntos_subscription.payment_method == 'bank_billet'
   end
 
   def postback_url
