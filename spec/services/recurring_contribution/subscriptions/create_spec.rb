efdf0ef68f747045b780a3ed2cfa4e8a4fefab74
Igor Fernandes <igorrfc10@gmail.com>
2016-12-26 13:33:34 -0300

    Update all RecurringContribution::Pagarme's cassetes
    passing the credit_card information only through the card_id and not by the credit_card_hash anymore.

--- spec/services/recurring_contribution/subscriptions/create_spec.rb
+++ spec/services/recurring_contribution/subscriptions/create_spec.rb
@@ -31,7 +31,10 @@ RSpec.describe RecurringContribution::Subscriptions::Create do
       context "when the payment_method is bank_billet" do
         let(:payment_method) { 'bank_billet' }
         let(:pagarme_api_response) { build_subscription_mock(payment_method, pagarme_plan) }
-        let(:juntos_subscription) { create(:subscription, plan: juntos_plan, project: recurring_project, user: user, payment_method: payment_method) }
+        let(:juntos_subscription) do
+          create(:subscription, plan: juntos_plan, project: recurring_project, user: user,
+                 payment_method: payment_method)
+        end
         subject { RecurringContribution::Subscriptions::Create.process(juntos_subscription) }
 
         it "should return an updated juntos' subscription" do
@@ -46,7 +49,7 @@ RSpec.describe RecurringContribution::Subscriptions::Create do
       subject { RecurringContribution::Subscriptions::Create.process(juntos_subscription) }
 
       context "when an invalid subscription attribute is sent" do
-        it "should raise an Pagarme::API::InvalidAttributeError" do
+        it "should raise a Pagarme::API::InvalidAttributeError" do
           allow_any_instance_of(RecurringContribution::Subscriptions::CreatePagarme)
             .to receive(:process).and_raise(Pagarme::API::InvalidAttributeError)
 
@@ -56,7 +59,7 @@ RSpec.describe RecurringContribution::Subscriptions::Create do
       end
 
       context "when the connection is lost" do
-        it "should raise an Pagarme::API::ConnectionError" do
+        it "should raise a Pagarme::API::ConnectionError" do
           allow_any_instance_of(RecurringContribution::Subscriptions::CreatePagarme)
             .to receive(:process).and_raise(Pagarme::API::ConnectionError)
 
