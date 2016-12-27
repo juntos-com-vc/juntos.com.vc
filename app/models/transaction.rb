f7e899d83661ab57b4c128a89260668cbd758d1a
Igor Fernandes <igorrfc10@gmail.com>
2016-12-27 16:27:11 -0300

    Update Transaction model
    using the active record enumerator instead of the gem enumerize.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,22 +1,12 @@
 class Transaction < ActiveRecord::Base
-  extend Enumerize
-
   validates_presence_of :transaction_code, :status, :amount,
                         :payment_method, :subscription_id
 
   belongs_to :subscription
   has_one    :project, through: :subscription
 
-  enumerize :status, in: {
-                           processing:      0,
-                           authorized:      1,
-                           paid:            2,
-                           refunded:        3,
-                           waiting_payment: 4,
-                           pending_payment: 5,
-                           refused:         6
-                         }
+  enum status: [ :pending_payment, :processing, :authorized, :paid, :refunded, :waiting_payment, :refused ]
 
-   enumerize :payment_method, in: { credit_card: 0, bank_billet: 1 }
+  enum payment_method: [ :credit_card, :bank_billet ]
 
 end
