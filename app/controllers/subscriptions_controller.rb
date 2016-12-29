2efb46f8b940f8c98357e22f3b1f146b53f9f3d4
Igor Fernandes <igorrfc10@gmail.com>
2016-12-29 16:00:52 -0300

    Change SubscriptionsController path

--- app/controllers/subscriptions_controller.rb
+++
@@ -1,9 +0,0 @@
-class SubscriptionsController < ApplicationController
-  def update_status
-    subscription = Subscription.find_by(subscription_code: params[:id])
-    RecurringContribution::UpdateStatus.process(request, subscription)
-    render nothing: true, status: :ok
-  rescue RecurringContribution::UpdateStatus::InvalidRequestError
-    render json: { error: 'invalid postback' }, status: :bad_request
-  end
-end
