42e2acf49f5e965b81dd139122127f0c89713fc9
Igor Fernandes <igorrfc10@gmail.com>
2016-12-30 16:53:37 -0300

    Update routes.rb file
    adding the post route for SubscriptionsController cancel action.

--- config/routes.rb
+++ config/routes.rb
@@ -214,4 +214,6 @@ Rails.application.routes.draw do
   post '/transaction/status/update' => 'transactions#update_status'
 
   post '/subscription/status/update' => 'projects/subscriptions#update_status'
+
+  post '/subscription/cancel' => 'projects/subscriptions#cancel'
 end
