6f8406222beeaf9d4745dfe923ae3c1c95d96067
Igor Fernandes <igorrfc10@gmail.com>
2017-02-10 18:02:01 -0300

    Update BankAccount model
    adding an association with its authorized_documents which are represented by the model S3Asset.

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,8 +1,13 @@
 class BankAccount < ActiveRecord::Base
   belongs_to :user
   belongs_to :bank
+  has_many   :authorization_documents
 
   validates :bank_id, :agency, :account, :owner_name, :owner_document, :account_digit, presence: true
+
+  accepts_nested_attributes_for :authorization_documents
+
+  scope :by_user, -> (user) { where(user_id: user.id) }
 
   def bank_code
     self.bank.code
--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -2,15 +2,34 @@ require 'rails_helper'
 
 RSpec.describe BankAccount, type: :model do
   describe "associations" do
-    it{ is_expected.to belong_to :user }
+    it { is_expected.to belong_to :user }
+    it { is_expected.to belong_to :bank }
+    it { is_expected.to have_many :authorization_documents }
   end
 
   describe "Validations" do
-    it{ is_expected.to validate_presence_of(:bank_id) }
-    it{ is_expected.to validate_presence_of(:agency) }
-    it{ is_expected.to validate_presence_of(:account) }
-    it{ is_expected.to validate_presence_of(:account_digit) }
-    it{ is_expected.to validate_presence_of(:owner_name) }
-    it{ is_expected.to validate_presence_of(:owner_document) }
+    it { is_expected.to validate_presence_of(:bank_id) }
+    it { is_expected.to validate_presence_of(:agency) }
+    it { is_expected.to validate_presence_of(:account) }
+    it { is_expected.to validate_presence_of(:account_digit) }
+    it { is_expected.to validate_presence_of(:owner_name) }
+    it { is_expected.to validate_presence_of(:owner_document) }
+  end
+
+  describe "scopes" do
+    describe ".by_user" do
+      let(:user) { create(:user) }
+      let(:user_bank_accounts) { create_list(:bank_account, 2, user: user) }
+
+      before do
+        create_list(:bank_account, 4)
+      end
+
+      subject { BankAccount.by_user(user) }
+
+      it "returns only the user's bank accounts" do
+        expect(subject).to match_array user_bank_accounts
+      end
+    end
   end
 end
