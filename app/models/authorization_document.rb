aa975c564ba40a16d458e0269d73f78725f356e9
Igor Fernandes <igorrfc10@gmail.com>
2017-03-06 14:38:03 -0300

    Update the AuthorizationDocument model
    making the following changes:
      - Enabling polymorphic associations with this model;
      - Including the legal_entity's required documents to the categories enum;
      - Validating the attachment url presence;

--- app/models/authorization_document.rb
+++ app/models/authorization_document.rb
@@ -1,10 +1,38 @@
 class AuthorizationDocument < ActiveRecord::Base
-  belongs_to :bank_account
+  belongs_to :authable, polymorphic: true
   has_one    :attachment, as: :attachmentable
 
   validates_presence_of :expires_at
 
+  validate :attachment_present
+
   accepts_nested_attributes_for :attachment
 
-  enum category: [:uncategorized, :bank_authorization, :organization_authorization]
+  enum category: [
+                  :uncategorized,
+                  :bank_authorization,
+                  :organization_authorization,
+                  :bylaw_registry,
+                  :last_general_meeting_minute,
+                  :fiscal_council_report,
+                  :directory_proof,
+                  :last_mandate_balance,
+                  :cnpj_card,
+                  :certificates,
+                  :last_year_activities_report,
+                  :organization_current_plan,
+                  :federal_tributes_certificate,
+                  :federal_court_debt_certificate,
+                  :manager_id
+                ]
+
+  def attachment_present
+    assign_document_error(category_i18n) unless attachment.url.present?
+  end
+
+  private
+
+  def assign_document_error(doc_category)
+    errors.add(:attachment, :without_attachment, category: doc_category)
+  end
 end
