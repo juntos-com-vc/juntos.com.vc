365915e59ff2037e216725aea8e1b27cf86a2515
Igor Fernandes <igorrfc10@gmail.com>
2017-03-06 14:36:40 -0300

    Update the Attachment model
    removing the url attribute validation.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,3 @@
 class Attachment < ActiveRecord::Base
   belongs_to :attachmentable, polymorphic: true
-
-  validates_presence_of :url
 end
--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Attachment, type: :model do
   describe "associations" do
     it { is_expected.to belong_to :attachmentable }
   end
-
-  describe "validations" do
-    it { is_expected.to validate_presence_of(:url) }
-  end
 end
