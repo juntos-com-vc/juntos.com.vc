84927181fac4d7085a5a07d45d89c1525f194aa1
Igor Fernandes <igorrfc10@gmail.com>
2017-03-14 15:42:22 -0300

    Update the BankAccountDecorator
    adding methods to fit the numbers of the agency and account number with their digits.

--- app/decorators/bank_account_decorator.rb
+++ app/decorators/bank_account_decorator.rb
@@ -8,4 +8,20 @@ class BankAccountDecorator < Draper::Decorator
   def bank_code
     source.bank.code
   end
+
+  def agency
+    return agency_with_digit if source.agency_digit
+
+    source.agency
+  end
+
+  def account
+    "#{source.account}-#{source.account_digit}"
+  end
+
+  private
+
+  def agency_with_digit
+    "#{source.agency}-#{source.agency_digit}"
+  end
 end
--- spec/decorators/bank_account_decorator_spec.rb
+++ spec/decorators/bank_account_decorator_spec.rb
@@ -22,4 +22,34 @@ RSpec.describe BankAccountDecorator do
       expect(subject).to match '1010'
     end
   end
+
+  describe "#agency" do
+    subject { bank_account.agency }
+
+    context "when the agency has a digit" do
+      let(:bank_account) { build(:bank_account, agency: '1111', agency_digit: '11').decorate }
+
+      it "returns the agency number plus the agency digit" do
+        expect(subject).to match '1111-11'
+      end
+    end
+
+    context "when the agency does not have a digit" do
+      let(:bank_account) { build(:bank_account, agency: '1111', agency_digit: nil).decorate }
+
+      it "returns the agency number" do
+        expect(subject).to match '1111'
+      end
+    end
+  end
+
+  describe "#account" do
+    let(:bank_account) { build(:bank_account, account: '2222', account_digit: '22').decorate }
+
+    subject { bank_account.account }
+
+    it "returns the account number plus the account digit" do
+      expect(subject).to match '2222-22'
+    end
+  end
 end
