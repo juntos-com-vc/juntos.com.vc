d33b4f7d9fc9cc92a2a81bfe874b942fbae7fb68
Maychell <maychellfernandes@hotmail.com>
2017-04-07 12:58:23 -0300

    Allow user to get back to previous page after signing in

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, :type => :controller do
+  describe "POST create" do
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+      session[:return_to] = return_to
+    end
 
+    context "when there is a redirect path set" do
+      let(:return_to) { '/how-it-works' }
+
+      it "redirects to the redirect path set page" do
+        post :create, user: attributes_for(:user)
+
+        is_expected.to redirect_to '/how-it-works'
+      end
+    end
+
+    context "when there is no return back set" do
+      let(:return_to) { nil }
+
+      it "redirects to the sign-up success page" do
+        post :create, user: attributes_for(:user)
+
+        is_expected.to redirect_to sign_up_success_path
+      end
+    end
+  end
 end
