655f22b7ac1676fcad86ec3b994bbd7cee1176b0
João Pinho <pinhojoao@gmail.com>
2017-05-22 18:48:45 -0300

    Registration sempre redireciona para https

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
+  before_filter :verify, :only => [:new]
+
+  def verify
+  	if request.protocol == 'http://' && ENV['ENVIRONMENT'] == 'production'
+  		redirect_to base_url = "https://secure.juntos.com.vc/#{params[:locale]}/sign_up"
+  	end
+  end
 
   def after_sign_up_path_for(resource)
     # add session information for tags
