0ee6673a7d1bfb6ddcc9ebc61102197ad9363c33
João Pinho <pinhojoao@gmail.com>
2017-06-07 19:51:43 -0300

    Adicionando possibilidade de inserir numeros no permalink

--- app/assets/javascripts/app/project_form.js
+++ app/assets/javascripts/app/project_form.js
@@ -7,7 +7,7 @@ App.addChild('ProjectForm', _.extend({
 
   activate: function(){
     this.setupForm();
-    this.$el.find('#project_permalink').attr('pattern', '[a-zA-Z|-]+');
+    this.$el.find('#project_permalink').attr('pattern', '[a-zA-Z0-9|-]+');
   }
 
 }, Skull.Form));
