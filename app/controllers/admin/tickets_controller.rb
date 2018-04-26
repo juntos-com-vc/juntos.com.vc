d13538c15afc5260b488d881eedcb6e2f40a50f5
João Pinho <pinhojoao@gmail.com>
2018-04-26 00:42:37 -0300

    Estilizando página de sorteio simples

--- app/controllers/admin/tickets_controller.rb
+++ app/controllers/admin/tickets_controller.rb
@@ -2,10 +2,8 @@ class Admin::TicketsController < Admin::BaseController
   layout 'juntos_bootstrap'
 
   def shuffle
-    puts "------------>>>>>>  <<<<<<-----------"
-    project = Project.by_permalink_and_available(params[:permalink]).first!
-    puts "#### ------------>>>>>>" + project.name + "<<<<<<-----------"
-    sql = "SELECT * FROM tickets WHERE project_id = #{project.id} OFFSET floor(random()*(SELECT count(*) FROM tickets WHERE project_id = #{project.id})) LIMIT 1"
+    @project = Project.by_permalink_and_available(params[:permalink]).first!
+    sql = "SELECT * FROM tickets WHERE project_id = #{@project.id} OFFSET floor(random()*(SELECT count(*) FROM tickets WHERE project_id = #{@project.id})) LIMIT 1"
     @ticket = Ticket.find_by_sql(sql)
   end
 
